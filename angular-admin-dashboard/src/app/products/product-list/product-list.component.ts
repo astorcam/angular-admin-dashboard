f4731beecbb3f3b62f1c0692e0cc324fd6f5dea6
Astorcam <62352203+astorcam@users.noreply.github.com>
2025-09-15 16:28:49 +0200

    feat: most sold category funcitons developed

--- angular-admin-dashboard/src/app/products/product-list/product-list.component.ts
+++ angular-admin-dashboard/src/app/products/product-list/product-list.component.ts
@@ -16,29 +16,80 @@ productTableConfig:any={
   displayedColumns:[{}],
   dataSource:[{}]
 }
-totalProducts!:number;
 bestSeller: any = {};
 worstSeller: any = {};
+lowStock: any = {};
+mostSalesCategory: any = {};
 
 constructor(private productService: ProductService,
   private salesService: SaleService
 ){}
 
 ngOnInit(){
+  //best seller
   this.salesService.getBestSeller().subscribe(i=>{
     this.bestSeller.sales=i.totalSold;
     if (i.productId !== null){
       this.productService.getProductById(i.productId).subscribe(p=>this.bestSeller.name=p.name)
     }
   })
+
+  //worst seller
   this.salesService.getWorstSeller().subscribe(i=>{
     this.worstSeller.sales=i.totalSold;
     if (i.productId !== null){
       this.productService.getProductById(i.productId).subscribe(p=>this.worstSeller.name=p.name)
     }
   })
+
+  //most sales category
+  this.salesService.getSales().subscribe(s=>{
+    const completed = s.filter(s => s.status === 'Completed');
+    const salesCategories: { [key: string]: number } = {}; 
+    const productSales: { [key: number]: number } = {}; 
+    completed.forEach(sale => {
+        if (!productSales[sale.productId]) {
+          productSales[sale.productId] = 0;
+        }
+        productSales[sale.productId] += sale.quantity;
+    })
+    for (const key in productSales) {
+      this.productService.getProductById(Number(key)).subscribe(p =>{
+         p.category
+        if (!salesCategories[p.category]) {
+            salesCategories[p.category] = 0;
+          }
+        salesCategories[p.category]+=productSales[key]
+      })
+    }
+    let mostSalesCategory="";
+    let maxQty = 0;
+      
+      for (const category in salesCategories) {
+        if (salesCategories[category] > maxQty) {
+          maxQty= salesCategories[category]
+          mostSalesCategory=category
+        }
+      }
+      this.mostSalesCategory.category = mostSalesCategory;
+      this.mostSalesCategory.sales = maxQty;
+  })
+
+
   this.productService.getProducts().subscribe(p =>{
-    this.totalProducts = p.length
+    //lowest stock
+    let lowerStock=p[0].stock;
+    let lowerStockName=p[0].name;
+    for (let product of p){
+      if(product.stock<lowerStock){
+        lowerStock=product.stock;
+        lowerStockName=product.name;
+      }  
+    }
+    
+    this.lowStock.stock=lowerStock;
+    this.lowStock.name=lowerStockName;
+    //product table config
     const keys = Object.keys(p[0]);
     this.productTableConfig.columns=keys;
     this.productTableConfig.displayedColumns = keys.map(k => ({
