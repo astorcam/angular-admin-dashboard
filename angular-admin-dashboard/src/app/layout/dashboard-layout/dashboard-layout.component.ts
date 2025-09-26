e7ea46963b0d09b09f9df4026f0252ba2fa54041
Astorcam <62352203+astorcam@users.noreply.github.com>
2025-09-26 15:52:28 +0200

    feat: fast footer developed

--- angular-admin-dashboard/src/app/layout/dashboard-layout/dashboard-layout.component.ts
+++ angular-admin-dashboard/src/app/layout/dashboard-layout/dashboard-layout.component.ts
@@ -3,11 +3,12 @@ import { UserMenuComponent } from '../../dashboard/layout/user-menu/user-menu.co
 import { GeneralMenuComponent } from '../../dashboard/layout/general-menu/general-menu.component'; 
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from "../../dashboard/layout/footer/footer.component";
 
 
 @Component({
   selector: 'app-dashboard-layout',
-  imports: [MatSidenavModule, RouterModule, UserMenuComponent, GeneralMenuComponent],
+  imports: [MatSidenavModule, RouterModule, UserMenuComponent, GeneralMenuComponent, FooterComponent],
   templateUrl: './dashboard-layout.component.html',
   styleUrl: './dashboard-layout.component.scss'
 })
