fcdb2893483acffb3a538d66d278573f00bb98bf
Astorcam <62352203+astorcam@users.noreply.github.com>
2025-10-14 14:03:06 +0200

    fis: fixed warning import

--- angular-admin-dashboard/src/app/users/user-form/user-form.component.ts
+++ angular-admin-dashboard/src/app/users/user-form/user-form.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-user-form',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.scss'
 })
